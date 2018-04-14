26a5bdbfbf1274516256363c679aac1f4ecd066d
Descole <jdescole@gmx.com>
2018-04-14 15:43:06 +0200

    Loading animations into ShapesCache

--- azran/client/azran.js
+++ azran/client/azran.js
@@ -5,6 +5,21 @@ require('pixi-animate');
 var MC = PIXI.animate.MovieClip;
 var G = PIXI.Graphics;
 
+let AnimationLoader = function()
+{
+    return function(resource, next)
+    {
+        let url = resource.url;
+        let data = resource.data;
+
+        if (data && url.search(/\animations\.json$/i) > -1) {
+            PIXI.animate.ShapesCache.add(resource.name, data);
+        }
+        next();
+    }
+}
+PIXI.loaders.Loader.addPixiMiddleware(AnimationLoader);
+
 var Scene = MC.e(function() {
     MC.call(this);
     console.log("== AZRAN SCENE BUILT ==");
