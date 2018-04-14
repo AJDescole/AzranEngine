a44c43896c2b4f9bffd67c6a80a78596501f00c0
Descole <jdescole@gmx.com>
2018-04-15 01:06:33 +0200

    Added tiny client for tests purposes

--- azran/client/azran.js
+++ azran/client/azran.js
@@ -24,17 +24,84 @@ PIXI.loaders.Loader.addPixiMiddleware(AnimationLoader);
 var Scene = MC.e(function() {
     MC.call(this);
 
-    let s = new P(
+    let player = new P(
         PIXI.animate.ShapesCache,
         "animations",
         "228"
     );
+    player.onDown = function(event) {
+        if (!player.isDown(event.keyCode)) {
+            ws.send(JSON.stringify(["sm", player.ID, event.keyCode, player.x, player.y]));
+        }
+    }
+    player.onUp = function(event) {
+        if (player.isDown(event.keyCode)) {
+            ws.send(JSON.stringify(["em", player.ID, event.keyCode, player.x, player.y]));
+        }
+    }
 
-    s.x = 400;
-    s.y = 400;
-    this.ac(s);
+    let characters = {};
+    let scene = this;
 
     console.log("== AZRAN SCENE BUILT ==");
+
+    let ws = new WebSocket("ws://localhost/azran_ws");
+
+    ws.onopen = function(event) {
+        console.log("connected");
+        scene.ac(player);
+        let hat = Math.floor(Math.random() * 23);
+        let gear = Math.floor(Math.random() * 19);
+        let ID = Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+        let x = Math.floor(Math.random() * 800);
+        let y = Math.floor(Math.random() * 800);
+
+        player.x = x;
+        player.y = y;
+        player.setHat(hat);
+        player.setGear(gear);
+        player.ID = ID;
+
+        console.log(ID, gear, hat, x, y);
+
+        ws.send(JSON.stringify(["join", ID, gear, hat, x, y]));
+    }
+
+    ws.onmessage = function(event) {
+        let e = JSON.parse(event.data);
+        let action = e[0];
+
+        if (action == "join") {
+            let ID = e[1];
+            let char = new A(
+                PIXI.animate.ShapesCache,
+                "animations",
+                "228"
+            );
+            char.setGear(e[2]);
+            char.setHat(e[3]);
+            char.x = e[4];
+            char.y = e[5];
+            char.ID = ID;
+
+            characters[ID] = char;
+            scene.ac(char);
+        } else if (action == "sm" || action == "em") {
+            let ID = e[1];
+            let char = characters[ID];
+            let key = e[2];
+
+            char.x = e[3];
+            char.y = e[4];
+            char.keys[key] = (action == "sm");
+        } else if (action == "quit") {
+            let ID = e[1];
+            let char = characters[ID];
+            scene.removeChild(char);
+            delete characters[ID];
+        }
+        console.log("received", event.data);
+    }
 });
 
 Scene.assets = {
