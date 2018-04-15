f80a134eca1c31fd5cff7c28babf9032de8af91f
Descole <jdescole@gmx.com>
2018-04-15 18:41:18 +0200

    Updated main with previous dev

--- azran/client/azran.js
+++ azran/client/azran.js
@@ -2,9 +2,8 @@
 require('pixi.js');
 require('pixi-animate');
 
-var MC = PIXI.animate.MovieClip;
-var A = require('./avatar.js').Avatar;
-var P = require('./player.js').Player;
+var MovieClip = PIXI.animate.MovieClip;
+var Client = require('./client.js').Client;
 
 let AnimationLoader = function()
 {
@@ -21,87 +20,16 @@ let AnimationLoader = function()
 }
 PIXI.loaders.Loader.addPixiMiddleware(AnimationLoader);
 
-var Scene = MC.e(function() {
-    MC.call(this);
+var Scene = MovieClip.e(function() {
+    MovieClip.call(this);
 
-    let player = new P(
-        PIXI.animate.ShapesCache,
-        "animations",
-        "228"
-    );
-    player.onDown = function(event) {
-        if (!player.isDown(event.keyCode)) {
-            ws.send(JSON.stringify(["sm", player.ID, event.keyCode, player.x, player.y]));
-        }
-    }
-    player.onUp = function(event) {
-        if (player.isDown(event.keyCode)) {
-            ws.send(JSON.stringify(["em", player.ID, event.keyCode, player.x, player.y]));
-        }
-    }
+    let sprite = new PIXI.Sprite.fromImage("bg.png");
+    sprite.x -= 600;
+    this.ac(sprite);
 
-    let characters = {};
-    let scene = this;
+    let client = new Client(this, PIXI.animate.ShapesCache, "animations", "228");
 
-    console.log("== AZRAN SCENE BUILT ==");
-
-    let ws = new WebSocket("ws://localhost/azran_ws");
-
-    ws.onopen = function(event) {
-        console.log("connected");
-        scene.ac(player);
-        let hat = Math.floor(Math.random() * 23);
-        let gear = Math.floor(Math.random() * 19);
-        let ID = Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-        let x = Math.floor(Math.random() * 800);
-        let y = Math.floor(Math.random() * 800);
-
-        player.x = x;
-        player.y = y;
-        player.setHat(hat);
-        player.setGear(gear);
-        player.ID = ID;
-
-        console.log(ID, gear, hat, x, y);
-
-        ws.send(JSON.stringify(["join", ID, gear, hat, x, y]));
-    }
-
-    ws.onmessage = function(event) {
-        let e = JSON.parse(event.data);
-        let action = e[0];
-
-        if (action == "join") {
-            let ID = e[1];
-            let char = new A(
-                PIXI.animate.ShapesCache,
-                "animations",
-                "228"
-            );
-            char.setGear(e[2]);
-            char.setHat(e[3]);
-            char.x = e[4];
-            char.y = e[5];
-            char.ID = ID;
-
-            characters[ID] = char;
-            scene.ac(char);
-        } else if (action == "sm" || action == "em") {
-            let ID = e[1];
-            let char = characters[ID];
-            let key = e[2];
-
-            char.x = e[3];
-            char.y = e[4];
-            char.keys[key] = (action == "sm");
-        } else if (action == "quit") {
-            let ID = e[1];
-            let char = characters[ID];
-            scene.removeChild(char);
-            delete characters[ID];
-        }
-        console.log("received", event.data);
-    }
+    client.connect("ws://localhost/azran_ws");
 });
 
 Scene.assets = {
@@ -110,7 +38,7 @@ Scene.assets = {
 };
 
 window.onload = function() {
-    let app = new PIXI.animate.Scene(800, 800, {
+    let app = new PIXI.animate.Scene(1200, 800, {
         view: document.getElementById("stage"),
         backgroundColor: 0xFFFFFF,
         antialias: true
