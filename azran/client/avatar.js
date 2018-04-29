b3f0ec4ffad22f4b2958f4c8587102d4d655a4aa
Descole <jdescole@gmx.com>
2018-04-29 23:43:36 +0200

    Avatar speed is now 3 instead of 2

--- azran/client/avatar.js
+++ azran/client/avatar.js
@@ -10,7 +10,7 @@ class Avatar extends PIXI.animate.MovieClip {
         super();
         this.animations = ["inactive", "up", "down", "right", "left"];
         this.keys = {};
-        this.speed = 2;
+        this.speed = 3;
 
         this.character = MCF(library, animationfield, symbol);
         this.setAnimation(this.animation);
