44e2efa32d98a8eb5ec7fac72bc30a11e2b4d670
Descole <jdescole@gmx.com>
2018-04-29 23:44:46 +0200

    Added MCF logger + added shortcuts for multiple recurrent commands

--- azran/client/MovieClipFactory.js
+++ azran/client/MovieClipFactory.js
@@ -2,7 +2,12 @@
 require('pixi.js');
 require('pixi-animate');
 
-let MovieClipFactory = function(library, animationfield, symbolfield) {
+function MCFLogger(...args)
+{
+    //console.log(...args);
+}
+
+let MovieClipFactory = function(library, animationfield, symbolfield, prefix = '#') {
     let animations = library[animationfield];
     let symbol = animations[symbolfield];
 
@@ -11,47 +16,91 @@ let MovieClipFactory = function(library, animationfield, symbolfield) {
     let Commands = {
         // build brand new movieclip
         b: function(options, duration, loop, framerate, labels) {
+            MCFLogger(prefix, "b", options, duration, loop, framerate, labels);
             return new PIXI.animate.MovieClip(options, duration, loop, framerate, labels);
         },
         // new symbol
         ns: function(obj, field, library, animationfield, symbolfield) {
-            obj[field] = MovieClipFactory(library, animationfield, symbolfield);
+            MCFLogger(prefix, "ns", field, animationfield, symbolfield);
+            obj[field] = MovieClipFactory(library, animationfield, symbolfield, prefix+"#");
             return obj;
         },
         // new graphics
         ng: function(obj, field, library, shapefield, index) {
+            MCFLogger(prefix, "ng", field, shapefield, index);
             obj[field] = (new PIXI.Graphics).d(library[shapefield][index]);
             return obj;
         },
         // add Timed Child
         at: function(obj, field, startFrame, duration, keyframes) {
+            MCFLogger(prefix, "at", field, startFrame, duration, keyframes);
             return obj.at(obj[field], startFrame, duration, keyframes);
         },
         // add Child
-        ac: function(obj, field) {
-            obj.ac(obj[field]);
+        ac: function(obj, ...fields) {
+            MCFLogger(prefix, "ac", fields);
+            for (let key in fields) {
+                let field = fields[key];
+                obj.ac(obj[field]);
+            }
             return obj;
         },
         // set Transform
-        t: function(obj, x, y, scaleX, scaleY, skewX, skewY, pivotX, pivotY) {
-            return obj.t(x, y, scaleX, scaleY, skewX, skewY, pivotX, pivotY);
+        t: function(obj, ...args) {
+            MCFLogger(prefix, "t", args);
+            return obj.t(...args);
         },
         // set Transform to subobject
-        tf: function(obj, field, x, y, scaleX, scaleY, skewX, skewY, pivotX, pivotY) {
-            obj[field].t(x, y, scaleX, scaleY, skewX, skewY, pivotX, pivotY);
+        tf: function(obj, field, ...args) {
+            MCFLogger(prefix, "tf", field, args);
+            obj[field].t(...args);
             return obj;
         },
         gs: function(obj, label) {
+            MCFLogger(prefix, "gs", label);
             obj.gotoAndStop(label);
             return obj;
         },
         // comments
         c: function(obj, ...data) {
-            console.log(data);
+            MCFLogger(prefix, "comments:", data);
             return obj;
         },
         // nullified
         n: function(obj, ...data) {
+            MCFLogger(prefix, "nullified:", data);
+            return obj;
+        },
+
+        // new graphics, add child and transform
+        ngac: function(obj, library, shapefield, index, ...transform) {
+            MCFLogger(prefix, "ngac", shapefield, index, transform);
+            let graphics = (new PIXI.Graphics).d(library[shapefield][index]);
+            if (transform.length > 0)
+                graphics.t(...transform);
+            obj.ac(graphics);
+            return obj;
+        },
+        // new symbol, add child and transform
+        nsac: function(obj, library, animationfield, symbolfield, ...transform) {
+            MCFLogger(prefix, "nsac", animationfield, symbolfield, transform);
+            let symbol = MovieClipFactory(library, animationfield, symbolfield);
+            if (transform.length > 0)
+                symbol.t(...transform);
+            obj.ac(symbol);
+            return obj;
+        },
+        // new graphics and add timed child
+        ngat: function(obj, library, shapefield, index, ...args) {
+            MCFLogger(prefix, "ngat", shapefield, index, args);
+            let graphics = (new PIXI.Graphics).d(library[shapefield][index]);
+            obj.at(graphics, ...args);
+            return obj;
+        },
+        nsat: function(obj, library, animationfield, symbolfield, ...args) {
+            MCFLogger(prefix, "nsat", animationfield, symbolfield, args);
+            let symbol = MovieClipFactory(library, animationfield, symbolfield);
+            obj.at(symbol, ...args);
             return obj;
         }
     }
@@ -64,6 +113,14 @@ let MovieClipFactory = function(library, animationfield, symbolfield) {
             MC = Commands.ns(MC, args[0], library, animationfield, args[1]);
         } else if (c == "ng") {
             MC = Commands.ng(MC, args[0], library, args[1], args[2]);
+        } else if (c == "nsac") {
+            MC = Commands.nsac(MC, library, animationfield, ...args);
+        } else if (c == "ngac") {
+            MC = Commands.ngac(MC, library, ...args);
+        } else if (c == "nsat") {
+            MC = Commands.nsat(MC, library, animationfield, ...args);
+        } else if (c == "ngat") {
+            MC = Commands.ngat(MC, library, ...args);
         } else if (c == "b") {
             MC = Commands.b(...args);
         } else {
